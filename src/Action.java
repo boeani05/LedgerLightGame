2f45b67a5be0306fbfcea8d9205de540b71d1452
boeani05 <flyingdoge@gmx.at>
2025-10-29 16:51:08 +0100

    New Functional Interface. Used it for first steps in Lambda Expressions

---
+++ src/Action.java
@@ -0,0 +1,8 @@
+@FunctionalInterface
+public interface Action<T> {
+    void perform(T item);
+
+    default void log(T item) {
+        System.out.println("Action performed on: " + item.getClass().getSimpleName());
+    }
+}
