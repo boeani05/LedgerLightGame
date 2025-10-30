d7d34a3e28645c2a80eb9ef56cb78f83348574fb
boeani05 <flyingdoge@gmx.at>
2025-10-30 10:27:35 +0100

    Displayed the annotations of the class inventory

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,13 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.lang.annotation.Annotation;
 
 public class Main {
     public static void main(String[] args) {
-        List<GameItem> gameItemList = new ArrayList<>();
-        GameItem weapon = new Weapon("Longsword", 2, 10, 15, ItemRarity.COMMON);
-        GameItem armor = new Armor("Chainmail", 21, 30, 15, ItemRarity.RARE);
+        Class<?> c = Inventory.class;
 
-        gameItemList.add(weapon);
-        gameItemList.add(armor);
+        Annotation[] annotations = c.getAnnotations();
 
-        Action<GameItem> printName = item -> System.out.println("Name: " + item.getName());
-        Action<GameItem> durabilityCheck = item -> {
-            if (item instanceof Durable durable) {
-                System.out.println(item.getName() + " durability: " + durable.getDurability());
-            }
-        };
-
-        for (GameItem gameItem : gameItemList) {
-            printName.perform(gameItem);
-            durabilityCheck.perform(gameItem);
+        for (Annotation annotation : annotations) {
+            System.out.println(annotation);
         }
     }
 }
